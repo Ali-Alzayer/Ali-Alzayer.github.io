2e75306252f5215d41ec94637815b530cb30fa48
Ali-Alzayer <89823176+Ali-Alzayer@users.noreply.github.com>
2021-09-30 15:13:06 -0400

    Awesome Jaguar Zippy ltd Company

--- itis3135/Simple web-based interaction.js
+++ itis3135/Simple web-based interaction.js
@@ -1,23 +1,40 @@
+let CompanyName = "Awesome Jaguar Zippy ltd Company"
+let name_ = prompt("Hey there visitor whats your name");
+let health = prompt("How you doing?");
+let today = new Date();
+document.getElementById("date").innerHTML += today.toDateString() +" and Time is " + today.toTimeString().slice(0, 8);
+document.getElementById("welcome").innerHTML += " <b>"+name_ +"</b> To "+ CompanyName + " <br/> We're glad you are doing "+health ;
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+document.getElementById("button1").onclick = calculateArea;
+document.getElementById("button2").onclick = calculatePeri;
+document.getElementById("button3").onclick = calculateAreaTriangel;
+document.getElementById("button4").onclick = calculateAreaSquare;
+document.getElementById("button5").onclick = calculatePeriSquare;
 
-today = mm + '/' + dd + '/' + yyyy;
-document.write(today);
-
-
-
-
-
-
-var first = document.getElementById("FirstName");     
-
-function welcome(){
-
-    var welcomeF = "Welcome" + first +" "+ last;
-    return welcomeF;
-
+function calculateArea(event) {
+    let width  = document.getElementById("width").value;
+    let height  = document.getElementById("height").value;
+    document.getElementById("area").innerHTML = "Area is " + width * height;
 }
 
+function calculatePeri(event) {
+    let width  = document.getElementById("width").value;
+    let height  = document.getElementById("height").value;
+    document.getElementById("peri").innerHTML = "Perimeter is " + 2*width + 2*height;
+}
+
+function calculateAreaTriangel(event) {
+    let width  = document.getElementById("base").value;
+    let height  = document.getElementById("heightT").value;
+    document.getElementById("areaT").innerHTML = "Area is " + width * height;
+}
+
+function calculateAreaSquare(event) {
+    let length  = document.getElementById("length").value;
+    document.getElementById("areaS").innerHTML = "Area is " + length * length;
+}
+
+function calculatePeriSquare(event) {
+    let length  = document.getElementById("length").value;
+    document.getElementById("periS").innerHTML = "Perimeter is " + 4*length;
+}
