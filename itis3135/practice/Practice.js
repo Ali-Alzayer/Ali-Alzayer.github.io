897e083f5114462fce769165d9bd38c5d885b80c
Ali-Alzayer <89823176+Ali-Alzayer@users.noreply.github.com>
2021-10-18 00:01:24 -0400

    updae

--- itis3135/practice/Practice.js
+++ itis3135/practice/Practice.js
@@ -1,16 +1,61 @@
-var $ = function(id) {
-	return document.getElementById(id);
-};
+"use strict";
 
-window.onload = function() {
+$(document).ready(function() {
 
+var total = 0;
 
-	//add onclick event handler for each image
+  
 
-	// for click event add item to order and update total
+// add hover handler and click handler to each image in table
 
-	// display order and total
+$("ul img").each(function() {
 
-	
-		
-}; // end onload
+  
+
+  
+
+// set up event handlers for each image, on hover
+
+$(this).hover(
+
+  
+
+); // end hover
+
+// set up event handlers for each image, when clicked
+
+$(this).click(function(evt) {
+
+  
+
+// add selection to textarea and update total
+
+// display order and total
+
+// cancel default event of the clicked link
+
+evt.preventDefault();
+
+  
+
+}); // end click
+
+}); // end each
+
+  
+
+// add click event handler for check out button
+
+$("#place_order").click(function() {
+
+}); // end click
+
+  
+
+// add click event handler for clear button
+
+$("#clear_order").click(function() {
+
+  
+
+}); // end click
